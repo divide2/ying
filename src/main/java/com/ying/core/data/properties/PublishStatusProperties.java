9c17ad7554c8e8770a68da0e0d30fa093cf65174
bvvy <bvvy@qq.com>
2018-08-16 22:23:23 +0800

    add publish

---
+++ src/main/java/com/ying/core/data/properties/PublishStatusProperties.java
@@ -0,0 +1,20 @@
+package com.ying.core.data.properties;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author bvvy
+ */
+@ConfigurationProperties(prefix = "dic.publish-status")
+@Component
+@Data
+public class PublishStatusProperties {
+
+    private String publish;
+    private String draft;
+
+
+}
+

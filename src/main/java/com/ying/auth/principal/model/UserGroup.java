ac5e0d93b51a82b4306cec72ba5965e1c9a6b768
Scarlett <chentingting@hitour.cc>
2018-08-27 21:12:41 +0800

    add group

---
+++ src/main/java/com/ying/auth/principal/model/UserGroup.java
@@ -0,0 +1,27 @@
+package com.ying.auth.principal.model;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * 用户的组
+ *
+ * @author bvvy
+ * @date 2018/8/25
+ */
+
+@Entity
+@Table(name = "sys_user_group")
+@Data
+public class UserGroup {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private Integer userId;
+    private Integer groupId;
+}

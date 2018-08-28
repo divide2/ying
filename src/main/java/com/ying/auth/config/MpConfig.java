c777045ea129265a063659ed5c51a8ae394b624b
bvvy <bvvy@qq.com>
2018-08-28 23:36:31 +0800

    MpLogin

---
+++ src/main/java/com/ying/auth/config/MpConfig.java
@@ -0,0 +1,13 @@
+package com.ying.auth.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
+
+/**
+ * @author bvvy
+ * @date 2018/8/28
+ */
+@EnableOAuth2Client
+@Configuration
+public class MpConfig {
+}

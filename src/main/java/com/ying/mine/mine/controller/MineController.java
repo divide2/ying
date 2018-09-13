e618abe8edb0474905a0fb3ba6041745696ca585
bvvy <bvvy@qq.com>
2018-09-13 22:12:18 +0800

    star

--- src/main/java/com/ying/mine/mine/controller/MineController.java
+++ src/main/java/com/ying/mine/mine/controller/MineController.java
@@ -1,5 +1,8 @@
 package com.ying.mine.mine.controller;
 
+import com.ying.basis.star.model.Star;
+import com.ying.basis.star.service.StarService;
+import com.ying.basis.star.vo.StarVO;
 import com.ying.core.er.Loginer;
 import com.ying.core.er.Responser;
 import com.ying.product.product.model.Product;
@@ -16,21 +19,28 @@ import org.springframework.web.bind.annotation.RestController;
  * @author bvvy
  * @date 2018/9/6
  */
-@RequestMapping("/v1/mine/product")
+@RequestMapping("/v1/mine")
 @RestController
-public class MineProductController {
+public class MineController {
 
     private final ProductService productService;
+    private final StarService starService;
 
-    public MineProductController(ProductService productService) {
+    public MineController(ProductService productService, StarService starService) {
         this.productService = productService;
+        this.starService = starService;
     }
 
-    @GetMapping("/find")
-    public ResponseEntity<Page<ProductVO>> find(Pageable pageable) {
+    @GetMapping("/products")
+    public ResponseEntity<Page<ProductVO>> product(Pageable pageable) {
             Page<Product> products = productService.findByUser(Loginer.userId(), pageable);
             return Responser.ok(products.map(ProductVO::of));
     }
 
+    @GetMapping("/stars")
+    public ResponseEntity<Page<StarVO>> star(Pageable pageable) {
+        Page<Star> stars = starService.find(pageable);
+        return Responser.ok(stars.map(StarVO::of));
+    }
     
 }

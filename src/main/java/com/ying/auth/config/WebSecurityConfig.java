feb00c0805074a32ef46d4baaaa67e1e74164a68
Scarlett <chentingting@hitour.cc>
2018-10-04 23:46:27 +0800

    add group

--- src/main/java/com/ying/auth/config/WebSecurityConfig.java
+++ src/main/java/com/ying/auth/config/WebSecurityConfig.java
@@ -5,10 +5,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import javax.servlet.Filter;
 
 
 /**
@@ -31,13 +35,19 @@ class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         return super.authenticationManagerBean();
     }
 
-
-
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
     }
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.addFilterBefore(filter(), BasicAuthenticationFilter.class);
+    }
+
+    private Filter filter() {
+
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {

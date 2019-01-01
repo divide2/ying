a154c99399a6b313c65efafb1196638dd2e21433
bvvy <bvvy@qq.com>
2019-01-01 18:36:49 +0800

    chat

--- src/main/java/com/ying/core/root/converter/Converter.java
+++ src/main/java/com/ying/core/root/converter/Converter.java
@@ -1,6 +1,9 @@
 package com.ying.core.root.converter;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 import java.util.function.Function;
@@ -11,6 +14,7 @@ import java.util.stream.Collectors;
  * @date 2018/12/28
  */
 @Data
+
 public class Converter<T> {
 
     private List<T> source;
--- src/main/java/com/ying/friend/service/impl/ChatServiceImpl.java
+++ src/main/java/com/ying/friend/service/impl/ChatServiceImpl.java
@@ -1,11 +1,13 @@
 package com.ying.friend.service.impl;
 
+import com.ying.auth.vo.UserVO;
 import com.ying.core.basic.service.impl.SimpleBasicServiceImpl;
+import com.ying.core.root.converter.Converter;
 import com.ying.friend.model.Chat;
 import com.ying.friend.repo.ChatRepository;
 import com.ying.friend.service.ChatService;
+import com.ying.friend.service.FriendConnectService;
 import com.ying.friend.vo.ChatVO;
-import io.swagger.models.auth.In;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -19,12 +21,26 @@ public class ChatServiceImpl extends SimpleBasicServiceImpl<Chat,String,ChatRepo
 
     private final ChatRepository chatRepository;
 
-    public ChatServiceImpl(ChatRepository chatRepository) {
+    private final FriendConnectService friendConnectService;
+
+    public ChatServiceImpl(ChatRepository chatRepository,
+                           FriendConnectService friendConnectService) {
         this.chatRepository = chatRepository;
+        this.friendConnectService = friendConnectService;
     }
 
     public List<ChatVO> listByUser(Integer userId) {
         List<Chat> chats = chatRepository.findByFromId(userId);
-
+        return Converter.of(chats).convert(chat -> {
+            UserVO user = friendConnectService.getUser(chat.getToId());
+            return ChatVO.builder()
+                    .id(chat.getId())
+                    .lastMessage(chat.getLastMessage())
+                    .lastTime(chat.getLastTime())
+                    .toAvatar(user.getAvatar())
+                    .toId(chat.getToId())
+                    .unreadCount(chat.getUnreadCount())
+                    .build();
+        });
     }
 }
